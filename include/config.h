8b2522f2ac6e6337ecdb890a0f5d469afd1c8a9d
proc0 <alanvx@gmail.com>
2026-01-15 23:09:36 -0800

    adding compile commands generation for LSP in cmakelists, adjusting emscripten runtime method export option

--- include/config.h
+++ include/config.h
@@ -3,5 +3,5 @@
 #define PROJECT_NAME "rayentt"
 #define VERSION "0.1.0"
 #define DEBUG 1
-#define DATE_BUILD "2025-12-08 13:36:54"
+#define DATE_BUILD "2026-01-15 23:04:00"
 #define DIR_ASSETS "assets"
